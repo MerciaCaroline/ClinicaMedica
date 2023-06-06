e10e03a3b4772e9df6e103129450180451fbc296
Mercia Martins <mercia.martins@acerto.com.br>
2023-06-05 23:55:38 -0300

    configura conexão com banco local

---
+++ src/PostgresDBConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresDBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/clinica-medica";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "WedbGf3a6ZHF3NwdKzGy";
+
+    public PostgresDBConnection() {
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão com o banco de dados estabelecida.");
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            throw e;
+        }
+        return connection;
+    }
+}
